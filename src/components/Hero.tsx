80c1c85e92a929335e13ad7074071c062660975e
Adam Lim <aedamjung@gmail.com>
2023-09-06 15:40:10 +0800

    Revert changes to previous state

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,33 +1,38 @@
-// pages/contact.tsx
 
-"use client";
 import React from 'react';
-import { CustomButton } from '.';
+import CustomButton from './CustomButton';
+import Image from 'next/image';
 
 const Hero = () => {
-  const handleScroll = () => {
+  const handleScroll = () => {};
 
-  }
   return (
-    <div className='hero'>
+    <div className="hero">
       <div className="flex-1 pt-36 padding-x padding-y">
-        <h1 className='hero__title'>
-          Find, book, or rent a software -- quickly and easily!
+        <h1 className="hero__title">
+          Find, book, or rent software -- quickly and easily!
         </h1>
 
-        <p className='hero__subtitle'>
+        <p className="hero__subtitle">
           Streamline your productivity with our effortless subscription process
         </p>
 
         <CustomButton
-        title="exploren cars"
-        containerStyles=""
-        handleClick={handleScroll}
+          title="Explore cars"
+          btnType="button"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src=""/>
+          <Image
+            src="/hero.png"
+            alt="hero"
+            fill
+            className="object-contain"
+          />
+          <div className="hero__image-overlay" />
         </div>
       </div>
     </div>
